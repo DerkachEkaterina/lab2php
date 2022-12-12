3c56877c86442dce5db8c5c34d9df1288b41ed09
DerkachEkaterina <derkachekaterina12345@gmail.com>
2022-12-12 13:27:12 +0200

    task1 task2

--- code/index.php
+++ code/index.php
@@ -1,2 +1,24 @@
 <?php
-echo phpinfo();
+function task1()
+{
+    $very_bad_unclear_name = "15 chicken wings";
+// Write your code here:
+    $order = &$very_bad_unclear_name;
+    $order .= ' and 5 cups of tea';
+// Don't change the line below
+    echo "\nYour order is: $very_bad_unclear_name.";
+}
+
+function task2()
+{
+    $var1 = 1;
+    echo $var1 . "</br>";
+    $var2 = 2;
+    echo "$var2" . "</br>";
+    $doubleVar = 6.78;
+    echo $doubleVar . "</br>";
+    echo 6 + 6 . "</br>";
+    $last_month = 1187.23;
+    $this_month = 1089.98;
+    echo $last_month - $this_month . "</br>";
+}
